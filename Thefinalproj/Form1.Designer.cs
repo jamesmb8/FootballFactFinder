0e57e9869e38dea02c482f5f842c0fa1fab26f45
JAMES BRIGGS <c2094665@hallam.shu.ac.uk>
2023-11-27 17:24:02 +0000

    Create layout

--- Thefinalproj/Form1.Designer.cs
+++ Thefinalproj/Form1.Designer.cs
@@ -28,68 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.LBTable = new System.Windows.Forms.ListBox();
+            this.BTNTable = new System.Windows.Forms.Button();
+            this.BTNTeam = new System.Windows.Forms.Button();
+            this.BTNFixtures = new System.Windows.Forms.Button();
+            this.BTNQuiz = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // LBTable
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(45, 87);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(282, 229);
-            this.listBox1.TabIndex = 0;
+            this.LBTable.FormattingEnabled = true;
+            this.LBTable.ItemHeight = 15;
+            this.LBTable.Location = new System.Drawing.Point(45, 87);
+            this.LBTable.Name = "LBTable";
+            this.LBTable.Size = new System.Drawing.Size(318, 319);
+            this.LBTable.TabIndex = 0;
             // 
-            // button1
+            // BTNTable
             // 
-            this.button1.Location = new System.Drawing.Point(67, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BTNTable.Location = new System.Drawing.Point(45, 58);
+            this.BTNTable.Name = "BTNTable";
+            this.BTNTable.Size = new System.Drawing.Size(75, 23);
+            this.BTNTable.TabIndex = 1;
+            this.BTNTable.Text = "Table";
+            this.BTNTable.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // BTNTeam
             // 
-            this.button2.Location = new System.Drawing.Point(172, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BTNTeam.Location = new System.Drawing.Point(126, 58);
+            this.BTNTeam.Name = "BTNTeam";
+            this.BTNTeam.Size = new System.Drawing.Size(75, 23);
+            this.BTNTeam.TabIndex = 2;
+            this.BTNTeam.Text = "Team";
+            this.BTNTeam.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // BTNFixtures
             // 
-            this.button3.Location = new System.Drawing.Point(287, 76);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.BTNFixtures.Location = new System.Drawing.Point(207, 58);
+            this.BTNFixtures.Name = "BTNFixtures";
+            this.BTNFixtures.Size = new System.Drawing.Size(75, 23);
+            this.BTNFixtures.TabIndex = 3;
+            this.BTNFixtures.Text = "Fixtures";
+            this.BTNFixtures.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // BTNQuiz
             // 
-            this.button4.Location = new System.Drawing.Point(413, 113);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.BTNQuiz.Location = new System.Drawing.Point(288, 58);
+            this.BTNQuiz.Name = "BTNQuiz";
+            this.BTNQuiz.Size = new System.Drawing.Size(75, 23);
+            this.BTNQuiz.TabIndex = 4;
+            this.BTNQuiz.Text = "Quiz";
+            this.BTNQuiz.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BTNQuiz);
+            this.Controls.Add(this.BTNFixtures);
+            this.Controls.Add(this.BTNTeam);
+            this.Controls.Add(this.BTNTable);
+            this.Controls.Add(this.LBTable);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,10 +98,10 @@
 
         #endregion
 
-        private ListBox listBox1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private ListBox LBTable;
+        private Button BTNTable;
+        private Button BTNTeam;
+        private Button BTNFixtures;
+        private Button BTNQuiz;
     }
 }
